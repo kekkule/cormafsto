178acc3046c1b8f54673e5e4abfa7ac72b5e7f64
Vladimir <yrovskyvladimir@gmail.com>
2023-03-16 18:32:16 +0300

    upd example

--- test/test_v3d.cpp
+++ test/test_v3d.cpp
@@ -7,7 +7,7 @@ int main () {
     V3D<double> A(1, 0, 0);
     V3D<double> B(0, 1, 0);
 
-    V3D<double> C = mul<double> (A, B);
+    V3D<double> C = mul (A, B);
     
     cout << C.get_x () << " " << C.get_y () << " " << C.get_z () << endl;
     cout << B.module () << endl;
